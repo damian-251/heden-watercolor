2137c7048da4df6f5e82316abb8683416b39ffc4
Damian <damianmd@outlook.com>
2022-04-25 18:48:59 +0200

    cambios menores

---
+++ app/Http/Controllers/ShoppingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * En este controlador irán los procesos relacinados con el
+ * proceso de compra
+ */
+class ShoppingController extends Controller
+{
+    //
+}
